0135026209351bed1fde2d8e25d92f755bf3b0e5
manpaco <116981639+manpaco@users.noreply.github.com>
2022-11-07 11:11:43 -0300

    creating crop rerctangle class

---
+++ include/croprectangle.h
@@ -0,0 +1,14 @@
+#ifndef CROPRECTANGLE_H
+#define CROPRECTANGLE_H
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+class CropRectangle : public wxControl {
+
+};
+
+#endif // CROPRECTANGLE_H
