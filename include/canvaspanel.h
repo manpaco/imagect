2f5572f323fb623e584647f81e90740658bf3405
manpaco <116981639+manpaco@users.noreply.github.com>
2022-11-07 14:28:36 -0300

    testing mouse events

--- include/canvaspanel.h
+++ include/canvaspanel.h
@@ -7,9 +7,20 @@
     #include <wx/wx.h>
 #endif
 
+#include "frameborder.h"
+
 class CanvasPanel: public wxScrolledWindow {
     public:
         CanvasPanel(wxWindow *parent, wxWindowID id);
+        void mouseMotionHandler(wxMouseEvent &);
+        void mouseLeftPress(wxMouseEvent &);
+        void mouseLeftRelease(wxMouseEvent &);
+
+    private:
+        wxButton *test;
+        wxBoxSizer *sz;
+
+        bool mousePressCrop = false;
 };
 
 #endif // CANVASPANEL_H
--- src/canvaspanel.cpp
+++ src/canvaspanel.cpp
@@ -1,7 +1,33 @@
 #include "canvaspanel.h"
-#include <wx/generic/panelg.h>
+#include "defs.h"
 
 CanvasPanel::CanvasPanel(wxWindow *parent, wxWindowID id) : 
         wxScrolledWindow() {
     Create(parent, id);
+
+    test = new wxButton(this, ict::CROP_RECT, "TEST");
+    sz = new wxBoxSizer(wxVERTICAL);
+    sz->Add(test);
+    SetSizerAndFit(sz);
+    test->Bind(wxEVT_MOTION, &CanvasPanel::mouseMotionHandler, this);
+    test->Bind(wxEVT_LEFT_DOWN, &CanvasPanel::mouseLeftPress, this);
+    test->Bind(wxEVT_LEFT_UP, &CanvasPanel::mouseLeftRelease, this);
+}
+
+void CanvasPanel::mouseMotionHandler(wxMouseEvent &event) {
+    if(mousePressCrop) {
+        // repaint crop rectangle
+        std::cout << "moving" << std::endl;
+    }
+    event.Skip();
+}
+
+void CanvasPanel::mouseLeftPress(wxMouseEvent &event) {
+    mousePressCrop = true;
+    std::cout << "press" << std::endl;
+}
+
+void CanvasPanel::mouseLeftRelease(wxMouseEvent &event) {
+    mousePressCrop = false;
+    std::cout << "release" << std::endl;
 }
