317bf45860a9bb31df7274f1343542d66e451519
manpaco <116981639+manpaco@users.noreply.github.com>
2022-12-10 01:00:32 -0300

    removing OptionsContainer (moved to optscontainer.h)

--- include/toolspanel.h
+++ include/toolspanel.h
@@ -24,55 +24,7 @@
 #endif
 
 #include "identifiersdef.h"
-
-struct OptionsContainer {
-    wxSize cropSize;
-    bool fixRatio;
-    float ratio;
-    int shapeChoice;
-    unsigned int strokeWidth;
-    wxColour strokeColour;
-    int strokeOff;
-    bool allowGrow;
-    int growChoice;
-    wxColour backColour;
-    wxString backImage;
-    int backBlur;
-
-    OptionsContainer() : 
-        cropSize(1, 1),
-        fixRatio(false),
-        ratio(0.0),
-        shapeChoice(0),
-        strokeWidth(0),
-        strokeColour(*wxBLACK),
-        strokeOff(0),
-        allowGrow(false),
-        growChoice(0),
-        backColour(*wxBLACK),
-        backImage(""),
-        backBlur(0)
-    {
-
-    }
-
-    bool operator==(const OptionsContainer &oc) const {
-        return cropSize == oc.cropSize &&
-            fixRatio == oc.fixRatio &&
-            shapeChoice == oc.shapeChoice &&
-            strokeWidth == oc.strokeWidth &&
-            strokeColour == oc.strokeColour &&
-            strokeOff == oc.strokeOff &&
-            allowGrow == oc.allowGrow &&
-            growChoice == oc.growChoice &&
-            backColour == oc.backColour &&
-            backImage == oc.backImage &&
-            backBlur == oc.backBlur;
-    }
-    bool operator!=(const OptionsContainer &oc) const {
-        return !(*this == oc);
-    }
-};
+#include "optscontainer.h"
 
 class ToolsPanel: public wxScrolledCanvas {
     public:
