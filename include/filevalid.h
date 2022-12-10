4c39e75f6e57408b0ae11d5249e1bf1e2623aef7
manpaco <116981639+manpaco@users.noreply.github.com>
2022-12-10 17:14:13 -0300

    adding tools for file validation

---
+++ include/filevalid.h
@@ -0,0 +1,22 @@
+#ifndef FILEVALID_H
+#define FILEVALID_H
+
+#include <string>
+#include "identifiersdef.h"
+
+const std::string pngExt = ".png";
+const std::string jpgExt = ".jpg";
+const std::string jpegExt = ".jpeg";
+const std::string pngWc = "PNG (*" + pngExt + ")|*" + pngExt;
+const std::string jpgWc = "JPG (*" + jpgExt +  ";*" + jpegExt + ")|*" + jpgExt + ";*" +jpegExt;
+const std::string importWc = "Image files (*" +
+    pngExt + ";*" +
+    jpgExt + ";*" +
+    jpegExt + ")|*" +
+        pngExt + ";*" +
+        jpgExt + ";*" +
+        jpegExt;
+
+ict::Extension extension(const std::string &filename);
+
+#endif // FILEVALID_H
