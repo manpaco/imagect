5056a66acd13bb61fe561285517cb7ddbf6d7255
manpaco <116981639+manpaco@users.noreply.github.com>
2022-12-10 00:58:03 -0300

    moving OptionsContainer and State to specific file

---
+++ include/optscontainer.h
@@ -0,0 +1,61 @@
+#ifndef OPTSCONTAINER_H
+#define OPTSCONTAINER_H
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+struct OptionsContainer {
+    wxSize cropSize;
+    bool fixRatio;
+    float ratio;
+    int shapeChoice;
+    unsigned int strokeWidth;
+    wxColour strokeColour;
+    int strokeOff;
+    bool allowGrow;
+    int growChoice;
+    wxColour backColour;
+    wxString backImage;
+    int backBlur;
+
+    OptionsContainer() : 
+        cropSize(1, 1),
+        fixRatio(false),
+        ratio(0.0),
+        shapeChoice(0),
+        strokeWidth(0),
+        strokeColour(*wxBLACK),
+        strokeOff(0),
+        allowGrow(false),
+        growChoice(0),
+        backColour(*wxBLACK),
+        backImage(""),
+        backBlur(0) {
+
+    }
+
+    bool operator==(const OptionsContainer &oc) const {
+        return cropSize == oc.cropSize &&
+            fixRatio == oc.fixRatio &&
+            shapeChoice == oc.shapeChoice &&
+            strokeWidth == oc.strokeWidth &&
+            strokeColour == oc.strokeColour &&
+            strokeOff == oc.strokeOff &&
+            allowGrow == oc.allowGrow &&
+            growChoice == oc.growChoice &&
+            backColour == oc.backColour &&
+            backImage == oc.backImage &&
+            backBlur == oc.backBlur;
+    }
+
+    bool operator!=(const OptionsContainer &oc) const {
+        return !(*this == oc);
+    }
+};
+
+typedef std::tuple<wxPoint, OptionsContainer> State;
+
+#endif // OPTSCONTAINER_H
