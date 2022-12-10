05388c870159e60c6b817b910e04669d97dc982b
manpaco <116981639+manpaco@users.noreply.github.com>
2022-12-10 01:04:09 -0300

    adding functions moved from mainframe.h

--- include/imgtools.h
+++ include/imgtools.h
@@ -3,6 +3,7 @@
 
 #include <Magick++.h>
 #include "identifiersdef.h"
+#include "optscontainer.h"
 
 const unsigned char depth8 = 255;
 const unsigned short depth16 = 65535;
@@ -12,8 +13,10 @@ void overlap(const Magick::Image &, Magick::Image &, bool, bool);
 Magick::Image extractArea(const Magick::Geometry &, const Magick::Image &);
 bool emptyIntersection(const Magick::Geometry &area, const Magick::Image &image);
 bool contains(const Magick::Geometry &, const Magick::Image &);
-unsigned char * extractDepth8Channel(const Magick::Image &, ict::Channel, bool);
+unsigned char * extractDepth8Channel(const Magick::Image &, ict::Channel, bool = true);
 unsigned char toDepth8(const unsigned short);
 unsigned short toDepth16(const unsigned char);
+Magick::Image composeState(const Magick::Image &img, const State &s);
+wxImage createImage(const Magick::Image &img);
 
 #endif // IMGTOOLS_H
