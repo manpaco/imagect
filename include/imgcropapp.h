7f0a85c47457d92bcf6ce8d646d9a39b33df4428
manpaco <116981639+manpaco@users.noreply.github.com>
2022-12-13 07:49:44 -0300

    implementing image initialization from command line param

--- include/imgcropapp.h
+++ include/imgcropapp.h
@@ -7,9 +7,26 @@
     #include <wx/wx.h>
 #endif
 
+#if wxUSE_CMDLINE_PARSER
+    #include <wx/cmdline.h>
+#endif
+
 class ImgCropApp: public wxApp {
     public:
         virtual bool OnInit();
+        virtual void OnInitCmdLine(wxCmdLineParser &parser);
+        virtual bool OnCmdLineParsed(wxCmdLineParser &parser);
+        virtual int OnExit();
+
+    private:
+        wxString *cliFile = nullptr;
 };
+
+static const wxCmdLineEntryDesc cliDesc[] = {
+    { wxCMD_LINE_PARAM,  nullptr, nullptr, "input image", wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
+    { wxCMD_LINE_NONE }
+};
+
+wxDECLARE_APP(ImgCropApp);
 
 #endif // ICTAPP_H
--- src/imgcropapp.cpp
+++ src/imgcropapp.cpp
@@ -4,8 +4,27 @@
 wxIMPLEMENT_APP(ImgCropApp);
 
 bool ImgCropApp::OnInit() {
-    MainFrame *imgCropFrame = new MainFrame();
+    if(!wxApp::OnInit()) return false;
+
+    MainFrame *imgCropFrame;
+
+    if(!cliFile) imgCropFrame = new MainFrame();
+    else imgCropFrame = new MainFrame(*cliFile);
     imgCropFrame->Show(true);
-    
+
     return true;
+}
+
+void ImgCropApp::OnInitCmdLine(wxCmdLineParser &parser) {
+    parser.SetDesc(cliDesc);
+}
+
+bool ImgCropApp::OnCmdLineParsed(wxCmdLineParser &parser) {
+    if(parser.GetParamCount() > 0) cliFile = new wxString(parser.GetParam());
+    return true;
+}
+
+int ImgCropApp::OnExit() {
+    if(cliFile) delete cliFile;
+    return 0;
 }
