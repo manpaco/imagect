128824180a48779804dffaf7f5792a9feccf3d09
manpaco <116981639+manpaco@users.noreply.github.com>
2023-01-24 21:22:46 -0300

    adding static const zoom values

--- include/zoomctrl.h
+++ include/zoomctrl.h
@@ -5,6 +5,7 @@ class wxButton;
 class wxTextCtrl;
 
 #include "wx/control.h"
+#include <array>
 
 class ZoomCtrl : public wxControl {
     public:
@@ -12,9 +13,10 @@ class ZoomCtrl : public wxControl {
         ~ZoomCtrl();
 
     private:
-        float zValues[6] = {0.5, 0.75, 1.0, 1.5, 2.5, 5.0};
         wxButton *zIn, *zOut;
         wxTextCtrl *percent;
 };
+
+static const std::array<float, 8> zValues = {0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0};
 
 #endif // ZOOMCTRL_H
