0c57146ab494fedc07a3a99e93c6ec5c69229a47
manpaco <116981639+manpaco@users.noreply.github.com>
2023-01-28 03:51:24 -0300

    adding preview code documentation

--- include/previewpanel.h
+++ include/previewpanel.h
@@ -8,13 +8,38 @@ class wxGridSizer;
 
 #include "wx/panel.h"
 
+/**
+ * Implementation of a preview panel
+ *
+ * Handle one image centered in the panel. Preview title.
+ * Support update.
+ */
 class PreviewPanel: public wxPanel {
 
     public:
+        /**
+         * Ctor.
+         */
         PreviewPanel(wxWindow *parent, wxWindowID id);
+
+        /**
+         * Construct a preview handling the given image.
+         */
         PreviewPanel(wxWindow *parent, wxWindowID id, wxBitmap &);
+
+        /**
+         * Update the handled image.
+         */
         void updatePreview(wxBitmap &);
+
+        /**
+         * Delete the internal image.
+         */
         void clear();
+
+        /**
+         * Dtor.
+         */
         ~PreviewPanel();
 
     private:
