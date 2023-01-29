bcfe77bb67bb9c8538f6f73b9ace4a5d743dfee3
manpaco <116981639+manpaco@users.noreply.github.com>
2023-01-29 10:58:07 -0300

    adding license notice for each file in the project

--- include/optscontainer.h
+++ include/optscontainer.h
@@ -1,3 +1,22 @@
+/**
+ *     Copyright (C) 2023 Acuilán Gabriel <acuilangabriel@gmail.com>
+ *
+ *     This file is part of Image Cropping Tool.
+ *
+ *     Image Cropping Tool is free software: you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or (at
+ *     your option) any later version.
+ * 
+ *     Image Cropping Tool is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ *     Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with Image Cropping Tool. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef OPTSCONTAINER_H
 #define OPTSCONTAINER_H
 
