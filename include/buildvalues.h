f8cafaf185cafbb5b9ff026fb90355361b47e9d7
manpaco <116981639+manpaco@users.noreply.github.com>
2023-01-29 18:25:04 -0300

    adding cmake generated file

---
+++ include/buildvalues.h
@@ -0,0 +1,11 @@
+#ifndef BUILDVALUES_H
+#define BUILDVALUES_H
+
+const char * const projectName = "ImageCT";
+const char * const versionString = "0.1.0";
+
+const unsigned int versionMajor = 0;
+const unsigned int versionMinor = 1;
+const unsigned int versionPatch = 0;
+
+#endif // BUILDVALUES_H
