22903739d36233cef380dfdc11359180c6ad2422
manpaco <116981639+manpaco@users.noreply.github.com>
2023-01-29 12:26:32 -0300

    changing project name in license notices

--- src/imagewindow.cpp
+++ src/imagewindow.cpp
@@ -1,20 +1,20 @@
 /**
  *     Copyright (C) 2023 Acuilán Gabriel <acuilangabriel@gmail.com>
  *
- *     This file is part of Image Cropping Tool.
+ *     This file is part of ImageCT.
  *
- *     Image Cropping Tool is free software: you can redistribute it and/or
- *     modify it under the terms of the GNU General Public License as published
- *     by the Free Software Foundation, either version 3 of the License, or (at
- *     your option) any later version.
+ *     ImageCT is free software: you can redistribute it and/or modify it under
+ *     the terms of the GNU General Public License as published by the Free
+ *     Software Foundation, either version 3 of the License, or (at your
+ *     option) any later version.
  * 
- *     Image Cropping Tool is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- *     Public License for more details.
+ *     ImageCT is distributed in the hope that it will be useful, but WITHOUT
+ *     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *     FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *     more details.
  *
  *     You should have received a copy of the GNU General Public License along
- *     with Image Cropping Tool. If not, see <https://www.gnu.org/licenses/>.
+ *     with ImageCT. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "imagewindow.h"
