ea1398ea8c8f6440f5ef0be6c59bda6cbbeed216
manpaco <116981639+manpaco@users.noreply.github.com>
2023-06-02 15:40:41 -0300

    expanding wxFlexGridSizer with growable col and row

--- src/scrolledcanvas.cpp
+++ src/scrolledcanvas.cpp
@@ -1,3 +1,22 @@
+/**
+ *     Copyright (C) 2023 Acuilán Gabriel <acuilangabriel@gmail.com>
+ *
+ *     This file is part of ImageCT.
+ *
+ *     ImageCT is free software: you can redistribute it and/or modify it under
+ *     the terms of the GNU General Public License as published by the Free
+ *     Software Foundation, either version 3 of the License, or (at your
+ *     option) any later version.
+ * 
+ *     ImageCT is distributed in the hope that it will be useful, but WITHOUT
+ *     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *     FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *     more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with ImageCT. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "scrolledcanvas.h"
 #include "canvasitem.h"
 #include "defs.h"
@@ -13,9 +32,13 @@ ScrolledCanvas::ScrolledCanvas(wxWindow *parent, wxWindowID id) : wxWindow(paren
     canvas = new wxPanel(this);
     vBar = new wxScrollBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSB_VERTICAL);
     hBar = new wxScrollBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSB_HORIZONTAL);
+    layout->AddGrowableCol(0);
+    layout->AddGrowableRow(0);
     layout->Add(canvas);
-    layout->Add(vBar);
-    layout->Add(hBar);
+    hBar->SetScrollbar(0, 50, 100, 50);
+    vBar->SetScrollbar(0, 50, 100, 50);
+    layout->Add(vBar, 1, wxEXPAND);
+    layout->Add(hBar, 1, wxEXPAND);
     SetSizer(layout);
     scaler = new Scaler(1, 1, ict::FLOOR_ST);
     testRectangle = new CanvasItem(wxRect(0, 0, 40, 40), nullptr, scaler, false);
