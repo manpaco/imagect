7dbb5528e0f467d771a7efc12f536169f896a806
manpaco <116981639+manpaco@users.noreply.github.com>
2024-01-30 19:43:50 -0300

    scaler refactor

--- include/scaler.hpp
+++ include/scaler.hpp
@@ -3,14 +3,10 @@
 
 #include <wx/gdicmn.h>
 #include "defs.hpp"
+#include <wx/geometry.h>
 
 const double minScaleFactor = 1.0 / 256;
 const double maxScaleFactor = 256;
-
-enum ScaleTarget {
-    IN_TARGET = 0,
-    OUT_TARGET
-};
 
 class Scaler {
 public:
@@ -18,7 +14,6 @@ public:
     Scaler(double xxf, double yyf, ict::ScaleType st);
 
     void setNewFactor(double xf, double yf);
-    void setScaleType(ict::ScaleType st);
     void getNewFactor(double *xf, double *yf) const;
     void getOldFactor(double *xof, double *yof) const;
     void getTransferFactor(double *xtf, double *ytf) const;
@@ -26,25 +21,23 @@ public:
     void clearTransfer();
     void addFactor(double axf, double ayf);
 
-    int scale(const int v, const ict::Dot d, const double f) const;
-    int scaleX(const int v, ict::Dot d) const;
-    int scaleY(const int v, ict::Dot d) const;
-    wxPoint scalePoint(const wxPoint &p, ict::Dot d) const;
-    wxSize scaleSize(const wxSize &s, ict::Dot d) const;
-    wxRect scaleRect(const wxRect &r, ict::Dot d) const;
+    double scaleX(const double v, ict::Dot d) const;
+    double scaleY(const double v, ict::Dot d) const;
+    wxPoint2DDouble scalePoint(const wxPoint2DDouble &p, ict::Dot d) const;
+    wxRect2DDouble scaleRect(const wxRect2DDouble &r, ict::Dot d) const;
 
-    int transferX(const int v, ict::Dot d) const;
-    int transferY(const int v, ict::Dot d) const;
-    wxPoint transferPoint(const wxPoint &p, ict::Dot d) const;
-    wxSize transferSize(const wxSize &s, ict::Dot d) const;
-    wxRect transferRect(const wxRect &r, ict::Dot d) const;
+    double transferX(const double v, ict::Dot d) const;
+    double transferY(const double v, ict::Dot d) const;
+    wxPoint2DDouble transferPoint(const wxPoint2DDouble &p, ict::Dot d) const;
+    wxRect2DDouble transferRect(const wxRect2DDouble &r, ict::Dot d) const;
 
     ~Scaler();
 
 private:
+    double scale(const double v, const ict::Dot d, const double f) const;
+
     double xxFactor = 1.0, yyFactor = 1.0;
     double xxOldFactor = 1.0, yyOldFactor = 1.0;
-    ict::ScaleType st = ict::FLOOR_ST;
 };
 
 #endif // !SCALER_H
