825c654f0234e2138104bf50d693e405f7f05963
manpaco <116981639+manpaco@users.noreply.github.com>
2024-02-05 18:57:36 -0300

    adding rectangle helper functions

---
+++ include/recthelper.hpp
@@ -0,0 +1,9 @@
+#ifndef RECTHELPER_H
+#define RECTHELPER_H
+
+#include <wx/geometry.h>
+
+void inflateRect(wxRect2DDouble *r, wxDouble d);
+void truncRect(wxRect2DDouble *r);
+
+#endif // !RECTHELPER_H
