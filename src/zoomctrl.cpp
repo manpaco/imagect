51ec4c9cf68b90ebe457728eb66fba815e37988c
manpaco <116981639+manpaco@users.noreply.github.com>
2024-07-20 20:05:39 -0300

    default buttons size

--- src/zoomctrl.cpp
+++ src/zoomctrl.cpp
@@ -7,10 +7,8 @@
 
 ZoomCtrl::ZoomCtrl(wxWindow *parent, wxWindowID id) :
     wxControl(parent, id, wxDefaultPosition, wxDefaultSize, wxNO_BORDER) {
-    zIn = new wxButton(this, ict::ZIN_BT, _("+"), wxDefaultPosition,
-                       wxDefaultSize, wxBU_EXACTFIT);
-    zOut = new wxButton(this, ict::ZOUT_BT, _("-"), wxDefaultPosition,
-                        zIn->GetSize());
+    zIn = new wxButton(this, ict::ZIN_BT, _("+"));
+    zOut = new wxButton(this, ict::ZOUT_BT, _("-"));
     percent = new wxTextCtrl(this, wxID_ANY, _("100%"), wxDefaultPosition,
                              wxDefaultSize, wxTE_READONLY | wxTE_CENTRE);
     wxBoxSizer *zSizer = new wxBoxSizer(wxHORIZONTAL);
